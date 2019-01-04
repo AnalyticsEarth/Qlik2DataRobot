a0fa45b59097cfa802cccba9263b1f460a69cea4
Steven Pressland <steven.pressland@qlik.com>
2019-01-04 14:38:34 +0000

    fix video autoplay in documentation

--- docs/video.js
+++ docs/video.js
@@ -34,7 +34,7 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-  event.target.playVideo();
+  //event.target.playVideo();
 }
 
 // 5. The API calls this function when the player's state changes.
